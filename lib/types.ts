c7bf1ca1b2c7607bb3865da1fa4a8729d89eb53b
mekail289 <mekail289@gmail.com>
2026-08-15 12:06:04 -0400

    Ajouter la quantité déjà disponible à chaque article

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,6 @@
 export type Level="2e année"|"4e année"|"6e année";
 export type Status="À acheter"|"J’en ai déjà"|"Réservé"|"Commandé"|"Acheté"|"Introuvable"|"À vérifier";
-export type Supply={id:string;level:Level;name:string;quantity:number;color?:string;format?:string;requirements?:string;mandatoryMerchant?:MerchantId;status:Status};
+export type Supply={id:string;level:Level;name:string;quantity:number;owned?:number;color?:string;format?:string;requirements?:string;mandatoryMerchant?:MerchantId;status:Status};
 export type Inventory={id:string;name:string;quantity:number;color?:string;format?:string;note?:string};
 export type MerchantId="jean-coutu"|"bureau-en-gros"|"commande-scolaire";
 export type Price={itemId:string;merchantId:MerchantId;packagePrice:number;packageQuantity:number;available:boolean};
